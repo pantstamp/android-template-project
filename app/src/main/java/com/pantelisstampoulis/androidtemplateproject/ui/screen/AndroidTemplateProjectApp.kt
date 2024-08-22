7eedcfd1aecc6003ead912735440533e9206d60d
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2024-08-22 20:35:35 +0300

    Fixes: scaffold padding issue

--- app/src/main/java/com/pantelisstampoulis/androidtemplateproject/ui/screen/AndroidTemplateProjectApp.kt
+++ app/src/main/java/com/pantelisstampoulis/androidtemplateproject/ui/screen/AndroidTemplateProjectApp.kt
@@ -1,17 +1,23 @@
 package com.pantelisstampoulis.androidtemplateproject.ui.screen
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.pantelisstampoulis.androidtemplateproject.navigation.AppDestination
 import com.pantelisstampoulis.androidtemplateproject.navigation.AppNavHost
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AndroidTemplateProjectApp() {
-    Scaffold(modifier = Modifier.fillMaxSize()) {
-        AppNavHost(startDestination = AppDestination.HomeScreenDestination)
+    Scaffold(modifier = Modifier.fillMaxSize()) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            AppNavHost(startDestination = AppDestination.HomeScreenDestination)
+        }
     }
 }
