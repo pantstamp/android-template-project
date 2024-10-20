9684b8678e636724badbf59b098b2f9a2a46f63c
Pantelis Stampoulis <pantelis.stampoulis@gmail.com>
2024-10-20 14:54:14 +0300

    Refactors: PullToRefreshLazyColumn

--- core/presentation/common-ui/src/main/kotlin/com/pantelisstampoulis/androidtemplateproject/common/ui/uicomponent/PullToRefreshLazyColumn.kt
+++ core/presentation/common-ui/src/main/kotlin/com/pantelisstampoulis/androidtemplateproject/common/ui/uicomponent/PullToRefreshLazyColumn.kt
@@ -9,13 +9,11 @@ import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,43 +28,32 @@ fun <T> PullToRefreshLazyColumn(
     lazyListState: LazyListState = rememberLazyListState(),
 ) {
     val pullToRefreshState = rememberPullToRefreshState()
+
     Box(
         modifier = modifier
-            .nestedScroll(pullToRefreshState.nestedScrollConnection),
+            .fillMaxSize(),
     ) {
-        LazyColumn(
-            state = lazyListState,
-            contentPadding = PaddingValues(8.dp),
-            modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            items(
-                items = items,
-                key = key,
-            ) {
-                content(it)
-            }
-        }
-
-        if (pullToRefreshState.isRefreshing) {
-            LaunchedEffect(true) {
-                onRefresh()
-            }
-        }
-
-        LaunchedEffect(isRefreshing) {
-            if (isRefreshing) {
-                pullToRefreshState.startRefresh()
-            } else {
-                pullToRefreshState.endRefresh()
-            }
-        }
-
-        PullToRefreshContainer(
+        // PullToRefreshBox adds the pull-to-refresh gesture support and content
+        PullToRefreshBox(
             state = pullToRefreshState,
-            modifier = Modifier
-                .align(Alignment.TopCenter),
-        )
+            isRefreshing = isRefreshing,
+            onRefresh = onRefresh,
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopCenter, // Align indicator to the top center
+        ) {
+            LazyColumn(
+                state = lazyListState,
+                contentPadding = PaddingValues(0.dp),
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                items(
+                    items = items,
+                    key = key,
+                ) {
+                    content(it)
+                }
+            }
+        }
     }
 }
